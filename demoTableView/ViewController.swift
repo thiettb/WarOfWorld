6eecdc41e18319238ab292790c27c1141a109cb3
Tung <tung@techmasters-iMac-2.local>
2017-07-24 14:38:41 +0700

    tungdx test git puush

--- demoTableView/ViewController.swift
+++ demoTableView/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var test = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
